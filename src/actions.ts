2e82a3e7b2595ddc3e941f5761b582eb4d11697f
Daven Quinn <dev@davenquinn.com>
2020-08-02 17:56:15 -0500

    Improved code style

--- src/actions.ts
+++ src/actions.ts
@@ -1,10 +1,15 @@
-import {nadirCameraPosition, ViewParams, CameraParams, flyToParams} from "./position"
-import {Props} from 'react'
-import {CameraFlyTo} from "resium"
+import {
+  nadirCameraPosition,
+  ViewParams,
+  CameraParams,
+  flyToParams,
+} from "./position";
+import { Props } from "react";
+import { CameraFlyTo } from "resium";
 
 enum ActiveMapLayer {
   CTX,
-  Hillshade
+  Hillshade,
 }
 
 type SetExaggeration = {
@@ -14,8 +19,8 @@ type SetExaggeration = {
 
 type SetMapLayer = {
   type: "set-map-layer";
-  value: ActiveMapLayer
-}
+  value: ActiveMapLayer;
+};
 
 enum DisplayQuality {
   High = "high",
@@ -29,25 +34,25 @@ type SetDisplayQuality = {
 
 type SetCameraPosition = {
   type: "set-camera-position";
-  value: ViewParams
-}
+  value: ViewParams;
+};
 
 type FlyToPosition = {
   type: "fly-to-position";
-  value: CameraParams
-  extra?: object
-}
+  value: CameraParams;
+  extra?: object;
+};
 
 type FlyTo = {
   type: "fly-to";
-  value: Props<typeof CameraFlyTo>
-}
+  value: Props<typeof CameraFlyTo>;
+};
 
 type FlyToNamedLocation = {
   type: "fly-to-named-location";
-  value: string
-  extra?: object
-}
+  value: string;
+  extra?: object;
+};
 
 type GlobeAction =
   | SetExaggeration
@@ -58,24 +63,36 @@ type GlobeAction =
   | FlyToNamedLocation
   | FlyTo;
 
+interface Positions {
+  [key: string]: CameraParams;
+}
+
 interface GlobeState {
+  positions: Positions;
   verticalExaggeration: number;
   displayQuality: DisplayQuality;
   mapLayer: ActiveMapLayer;
-  position: ViewParams|null;
+  position: ViewParams | null;
   flyToProps: Props<typeof CameraFlyTo>;
-  namedLocation: string|null;
+  namedLocation: string | null;
 }
 
 const destination = nadirCameraPosition(77.433, 18.411, 9);
 
 const initialState = {
+  positions: [],
   verticalExaggeration: 1,
   displayQuality: DisplayQuality.Low,
   mapLayer: ActiveMapLayer.CTX,
   position: null,
-  flyToProps: {destination, duration: 0, once: true},
-  namedLocation: null
+  flyToProps: { destination, duration: 0, once: true },
+  namedLocation: null,
+};
+
+const createInitialState = (args = {}) => {
+  let state = { ...initialState, ...args };
+  console.log(state);
+  return state;
 };
 
 const reducer = (state: GlobeState = initialState, action: GlobeAction) => {
@@ -85,23 +102,34 @@ const reducer = (state: GlobeState = initialState, action: GlobeAction) => {
     case "set-display-quality":
       return { ...state, displayQuality: action.value };
     case "fly-to-position": {
-      const value = flyToParams(action.value, action.extra)
-      return reducer(state, {type: 'fly-to', value});
+      const value = flyToParams(action.value, action.extra);
+      return reducer(state, { type: "fly-to", value });
     }
-    case 'fly-to-named-location':
-      const {value, extra} = action
-      if (value == state.namedLocation) return
-      const pos = action.positions[value]
-      return reducer(state, {type: "fly-to-position", value: pos, extra})
-    case 'fly-to':
+    case "fly-to-named-location":
+      const { value, extra } = action;
+      if (value == state.namedLocation) return state;
+      const pos = state.positions[value];
+      if (pos == null) return state;
+      return {
+        ...state,
+        namedLocation: value,
+        flyToProps: flyToParams(pos, action.extra),
+      };
+    case "fly-to":
       return { ...state, flyToProps: action.value };
     case "set-camera-position":
       return { ...state, position: action.value };
     case "set-map-layer":
-      return { ...state, activeMapLayer: action.value}
+      return { ...state, activeMapLayer: action.value };
     default:
       return state;
   }
 };
 
-export { reducer, GlobeAction, DisplayQuality, ActiveMapLayer };
+export {
+  reducer,
+  GlobeAction,
+  DisplayQuality,
+  ActiveMapLayer,
+  createInitialState,
+};
